2c0b27b1367922af38f27826df8a32aef869ec79
Alexander Faxa <alefa117@student.liu.se>
2013-12-02 13:47:08 +0100

    Improved multi core sort with smart pivoting for inc/dec/const input.

--- Lab3/sort.c
+++ Lab3/sort.c
@@ -42,9 +42,9 @@
 pthread_attr_t attr;
 
 struct array* array;
-int* b[NB_THREADS];
+int* temp_array[NB_THREADS];
 int current_index[NB_THREADS];
-int pivots[NB_THREADS-1];
+int pivots[NB_THREADS - 1];
 
 typedef struct {
   int from, to;
@@ -56,13 +56,6 @@ void swap(int* array, int ai, int bi) {
   const int c = array[ai];
   array[ai] = array[bi];
   array[bi] = c;
-}
-
-// Returns index of pivot (median of first, middle, last)
-int
-get_pivot_index(int* array, const int lower, const int upper) {
-  int middle = (lower + upper) / 2;
-  return middle;
 }
 
 void
@@ -84,14 +77,15 @@ seq_selection_sort(int* array, const int from, const int to) {
 // In place partitions array and returns the position of the pivot.
 int
 seq_partition(int* array, const int from, const int to) {
-  const int last = to - 1; // Index of last element.
-  const int pivot_index = get_pivot_index(array, from, last);
+  const int last = to - 1;  // Index of last element.
+  const int pivot_index = (from + to) / 2;
   const int pivot = array[pivot_index];
   swap(array, pivot_index, last);
 
   int left = from;
   int i;
   for (i = from; i < last; ++i) {
+    // To balance cases when elements are equal to pivot.
     if ( (i%2) ?
 	 array[i] <= pivot :
 	 array[i] < pivot) {
@@ -126,6 +120,7 @@ seq_quicksort_start(void* vargs) {
   return NULL;
 }
 
+// Thread-safe fetch and increment operation for shared variables.
 int
 fetch_and_add(int* variable, int value) {
   asm volatile(
@@ -136,23 +131,47 @@ fetch_and_add(int* variable, int value) {
   return value;
 }
 
+// Loops through the array to be sorted and puts every element in the right sub help array based on its relation to the pivots.
 void*
 fast_sort_partition(void* vargs) {
   parallel_args* args = (parallel_args*)vargs;
   int i;
   for (i = args->from; i < args->to; i++) {
     int j = 0;
-    int cur = args->array[i];
-    while (cur >= pivots[j]) {
+    const int cur = args->array[i];
+    const int offset = i % NB_THREADS;
+    while (cur > pivots[j] || (cur == pivots[j] && offset > j)) {
       ++j;
       if (j >= NB_THREADS - 1) {
 	break;
       }
     }
-    b[j][fetch_and_add(&current_index[j], 1)] = cur;
+    temp_array[j][fetch_and_add(&current_index[j], 1)] = cur;
   }
-
   return NULL;
+}
+
+// Selects NBTHREADS-1 pivots from the input array in a good way, sorts them and puts them in the pivot array.
+void
+find_pivots() {
+  const int num_pivots = NB_THREADS - 1;
+  const int sample_size = num_pivots + (6 * (num_pivots + 1));
+  
+  // Start by sampling values.
+  int candidates[sample_size];
+  
+  const int sample_interval = array->length / sample_size;
+  const int mid = sample_interval / 2;
+  int i;
+  for (i = 0; i < sample_size; i++) {
+    candidates[i] = array->data[i * sample_interval + mid];
+  }
+  // Sort them and pick pivots not close to each other.
+  seq_quicksort(candidates, 0, sample_size);
+
+  for (i = 0; i < num_pivots; i++) {
+    pivots[i] = candidates[(i * 7) + 6];
+  }
 }
 
 int
@@ -160,21 +179,20 @@ fast_sort_start(struct array* my_array) {
   pthread_attr_init(&attr);
   array = my_array;
 
-  const int length = array->length - (NB_THREADS - 1);
+  // Do not include the pivots in the array to be sorted.
+  const int length = array->length;
   const int size = length / NB_THREADS;
   int i;
   for (i = 0; i < NB_THREADS; i++) {
-    b[i] = malloc(array->length * sizeof(int));
+    // Worst case, all elements will end up in the same
+    // temporary array.
+    temp_array[i] = malloc(array->length * sizeof(int));
     current_index[i] = 0;
-    if (i != NB_THREADS-1)
-      swap(array->data, length+i, size * (i + 1));
   }
 
-  for (i = 0; i < NB_THREADS-1; i++) {
-    pivots[i] = array->data[array->length - 1 - i];
-  }
-  seq_selection_sort(pivots, 0, NB_THREADS-1);
+  find_pivots();
 
+  // Split the partitioning equally on all threads.
   pthread_t threads[NB_THREADS];
   parallel_args args[NB_THREADS];
   for (i = 0; i < NB_THREADS; i++) {
@@ -186,26 +204,14 @@ fast_sort_start(struct array* my_array) {
   for (i = 0; i < NB_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
-  for (i = 0; i < NB_THREADS - 1; i++) {
-    b[i][current_index[i]++] = pivots[i];
-  }
 
-  /* for (i = 0; i < NB_THREADS; i++) { */
-  /*   printf("b[%d]: \n", i); */
-  /*   int j; */
-  /*   for (j = 0; j < current_index[i]; j++) { */
-  /*     printf("%d ", b[i][j]); */
-  /*   } */
-  /*   printf("\n=================\n"); */
-  /* } */
-
-
+  // Copy elements back to original parallely.
   int start = 0;
   for (i = 0; i < NB_THREADS; i++) {
     args[i].from = 0;
     args[i].to = current_index[i];
     args[i].start = start;
-    args[i].array = b[i];
+    args[i].array = temp_array[i];
     pthread_create(&threads[i], &attr, seq_quicksort_start, (void*)&args[i]);
     start += current_index[i];
   }
